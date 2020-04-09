aafe34530e2ad8b2a88dbda51725ef7e6b96a09c
MiloAtGitHub <milozhong@hotmail.com>
2020-04-10 02:32:44 +0800

    不能瞎起工程名！！

--- mysql/testmysql.py
+++ mysql/testmysql.py
@@ -1,36 +1,51 @@
 """
-
 mysql测试
-
 """
-
 import mysql.connector
 from mysql.connector import errorcode
 
-# 连接数据库
-try:
-    cnx = mysql.connector.connect(
-        user='root',
-        password='1234',
-        host='59.108.67.5',
-        database='cucbm'
-    )
-except mysql.connector.Error as err:
-    if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-        print("Something wrong with your password.")
-    elif err.errno == errorcode.ER_BAD_DB_ERROR:
-        print("Database does not exist.")
+
+def connect_mysql(host, database, user, password, table):
+    """
+    这就是所谓的docstring？
+    """
+    # 连接数据库
+    try:
+        cnx = mysql.connector.connect(
+            user=user,
+            password=password,
+            host=host,
+            database=database
+        )
+    except mysql.connector.Error as err:
+        if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+            print("Something wrong with your password.")
+        elif err.errno == errorcode.ER_BAD_DB_ERROR:
+            print("Database does not exist.")
+        else:
+            print(err)
     else:
-        print(err)
-else:
-    query = "select * from dingqibaosong"
-    cursor = cnx.cursor()
-    cursor.execute(query)
-    for cur in cursor:
-        print(cur)
+        cursor = cnx.cursor()
+        cursor.execute("show tables")
+        table_lists = cursor.fetchall()
+        for table_list in table_lists:
+            print(table_list)
+        query = "select * from %s" % table
+        cursor.execute(query)
+        curs = cursor.fetchall()
+        cursor.close()
+        cnx.close()
+        return curs
 
-    cursor.close()
-    cnx.close()
-# print(cursor)
+
+def main():
+    """
+    打印表
+    """
+    rows = connect_mysql(host='59.108.67.5', user='root', password='1234', database='cucbm', table='dingqibaosong')
+    for row in rows:
+        print(row)
 
 
+if __name__ == '__main__':
+    main()
