a15203a90dac6f17c8a4de203b226851ccd46b7e
MiloAtGitHub <milozhong@hotmail.com>
2020-04-10 00:09:02 +0800

    不能瞎起工程名！！

--- mysql/testmysql.py
+++ mysql/testmysql.py
@@ -5,10 +5,32 @@ mysql测试
 """
 
 import mysql.connector
-cnx = mysql.connector.connect(
-    user = 'root',
-    password = '1234',
-    host = '59.108.67.5',
-    database = 'cucbm'
-)
-cnx.close()
+from mysql.connector import errorcode
+
+# 连接数据库
+try:
+    cnx = mysql.connector.connect(
+        user='root',
+        password='1234',
+        host='59.108.67.5',
+        database='cucbm'
+    )
+except mysql.connector.Error as err:
+    if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+        print("Something wrong with your password.")
+    elif err.errno == errorcode.ER_BAD_DB_ERROR:
+        print("Database does not exist.")
+    else:
+        print(err)
+else:
+    query = "select * from dingqibaosong"
+    cursor = cnx.cursor()
+    cursor.execute(query)
+    for cur in cursor:
+        print(cur)
+
+    cursor.close()
+    cnx.close()
+# print(cursor)
+
+
